7280bd5744c2e8bd39036d3efa33e52c28444ef5
GMohammadAli <gulammohammadali@gmail.com>
2026-02-05 16:38:59 +0530

    ResearcHub-Server: Updated client server url for cors issue

--- researchub-server/src/index.ts
+++ researchub-server/src/index.ts
@@ -15,7 +15,7 @@ const PORT = process.env.SERVER_PORT ?? 1025;
 const MONGO_DB_URL =
   process.env.MONGO_DB_URL ??
   "mongodb://localhost:27017/ai-document-summarizer";
-const CLIENT_APP_URL = process.env.VITE_API_URL ?? "http://localhost:3000";
+const CLIENT_APP_URL = process.env.CLIENT_APP_URL ?? "http://localhost:3000";
 
 const app = express();
 
